9ea005dea12850fa72e4dd00ac4565328d9c92d1
Dmitriy <plotnikovdi95@gmail.ru>
2018-11-02 16:55:07 +0300

    2 и 3 пункты

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => { 
+
     // main-слайдер
     let slides = document.querySelectorAll('.main-slider-item'),
         slideIndex = 1;
@@ -15,5 +16,51 @@ window.addEventListener('DOMContentLoaded', () => {
         let animate = () => slideIndex <= 1 ? plusSlides(1) : plusSlides(-1); 
        
         setInterval(animate, 4000);
+
+    // Модальные окна 
+
+    let buttonOrder = document.querySelectorAll('.button-design'),
+        popupDesign = document.querySelector('.popup-design'),
+        popupContent= popupDesign.querySelector('.popup-dialog'),
+        mainForm= popupDesign.querySelector('.form'),
+        popupClose = document.querySelectorAll('.popup-close'),
+        buttonConsultation = document.querySelectorAll('.button-consultation'),
+        popupConsultation = document.querySelector('.popup-consultation'),
+       
+
+        openModalOrder  = () => popupDesign.style.display = 'block',
+        closeModalOrder = () => { 
+            popupDesign.style.display = 'none';
+            popupConsultation.style.display = 'none'; 
+         } , 
+
+        openModalConsultation = () => popupConsultation.style.display = 'block';
+
+
         
+    buttonOrder.forEach( (item) => item.addEventListener('click', () => openModalOrder() ) );
+
+    popupClose.forEach( (item) => item.addEventListener('click', () => closeModalOrder() ) );
+    
+    
+    popupDesign.addEventListener('click', (e) =>  {
+
+        if (e.target.classList.contains('popup-design') ) {
+            closeModalOrder();
+        }
+
+    });
+
+    buttonConsultation.forEach( (item) => item.addEventListener('click', () => openModalConsultation() ) );
+
+
+
+    popupConsultation.addEventListener('click', (e) =>  {
+
+        if (e.target.classList.contains('popup-consultation') ) {
+            closeModalOrder();
+        }
+
+    });
+
 });
