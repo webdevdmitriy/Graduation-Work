df573a6cec8c922561cda16bc60f037db74b3daf
Dmitriy <plotnikovdi95@gmail.ru>
2018-11-02 13:33:57 +0300

    Главный слайдер

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => { 
+    // main-слайдер
+    let slides = document.querySelectorAll('.main-slider-item'),
+        slideIndex = 1;
+    
+        function showSlides(n) {
+
+            slides.forEach( (item) => item.style.display = 'none');
+            slides[slideIndex - 1].style.display = 'block';
+            slides[slideIndex - 1].className = 'animated slideInDown';
+        }
+        showSlides(slideIndex);
+
+        let  plusSlides = (n) => showSlides(slideIndex += n);
+        let animate = () => slideIndex <= 1 ? plusSlides(1) : plusSlides(-1); 
+       
+        setInterval(animate, 4000);
+        
+});
