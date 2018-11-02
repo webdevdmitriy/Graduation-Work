fdadf6afc959f20052555f4efefef0aadca61c0f
Dmitriy <plotnikovdi95@gmail.ru>
2018-11-02 22:38:33 +0300

    оооо

--- js/script.js
+++ js/script.js
@@ -21,12 +21,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     let buttonOrder = document.querySelectorAll('.button-design'),
         popupDesign = document.querySelector('.popup-design'),
-        popupContent= popupDesign.querySelector('.popup-dialog'),
+        // popupContent= popupDesign.querySelector('.popup-dialog'),
         mainForm= popupDesign.querySelector('.form'),
         popupClose = document.querySelectorAll('.popup-close'),
         buttonConsultation = document.querySelectorAll('.button-consultation'),
         popupConsultation = document.querySelector('.popup-consultation'),
+        popupContent = document.querySelectorAll('.popup-content'),
+        popupContentContent = document.querySelectorAll('.popup-content_content'),
+        mm = document.querySelectorAll('.popup-dialog'),
+
        
+      
 
         openModalOrder  = () => popupDesign.style.display = 'block',
         closeModalOrder = () => { 
@@ -36,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         openModalConsultation = () => popupConsultation.style.display = 'block';
 
-
+   
         
     buttonOrder.forEach( (item) => item.addEventListener('click', () => openModalOrder() ) );
 
@@ -62,5 +67,60 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     });
+
+    //Отправка форм
+
+
+  
+
+    let forms = document.querySelectorAll('form');
+
+    for(let i=0; i < forms.length; i++) {
+
+        forms[i].addEventListener('submit', function(event) {
+        event.preventDefault();
+
+    function postData() {
+        return new Promise( (resolve, reject) => {
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(forms[i]);
+
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+            request.send(json);
+
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState < 4) {
+                    resolve();
+                } else if (request.readyState === 4 && request.status == 200) {
+                   resolve();
+                } else {
+                    reject();
+                }
+            });
+        });
+    }
+    let consultations = document.querySelector('.consultation'),
+    consultationsContainer = consultations.querySelector('.container');
+    
+    let div = document.createElement('div');
+        div.innerHTML = '<img src="../img/radost_02.gif">'+'</br>'+'Спасибо, мы скоро с вами свяжемся';
+
+        console.log(popupContent.length);
+            postData()
+                .then ( 
+                popupContentContent.forEach( (item) =>item.style.display = 'none'), 
+                popupContent.forEach( (item) => item.appendChild(div) )
+                 );
+    
+        });      
+    }
 
 });
